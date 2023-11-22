5697c9fca9db0b2bd1bff9b574f1311c5f39be65
Dharma Rao S <s_dharmarao@yahoo.com>
2023-11-22 06:02:33 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>Dharma</h1>
-		<p><b>Openshift Batch Verizon New version </b></p>
+		<p><b>Openshift Batch Verizon New Version </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information </h2> 
